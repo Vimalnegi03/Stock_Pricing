ee380ba1be41217384874fd1be6a3a019d8ea990
Vimal <vm.21cs48@thdcihet.ac.in>
2025-07-06 12:17:13 +0530

    added pagination

--- backend/Repository/StockRepository.cs
+++ backend/Repository/StockRepository.cs
@@ -60,7 +60,9 @@ namespace backend.Repository
                     stocks = query.IsDescending ? stocks.OrderByDescending(s => s.CompanyName) : stocks.OrderBy(s => s.CompanyName);
                 }
             }
-            return await stocks.ToListAsync();
+            var skipNumber=(query.PageNumber-1)*query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
       public async Task<Stock?> GetByIdAsync(int id)
